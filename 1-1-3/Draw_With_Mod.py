5a4a2f1ca3244cd621040843ec378828f23dd898
15273 <ryan.mauro@vtsd.com>
2024-09-18 12:25:48 -0400

    Added alternating floor colors made 3 smaller towers.

--- 1-1-3/Draw_With_Mod.py
+++ 1-1-3/Draw_With_Mod.py
@@ -13,19 +13,25 @@ y = -150
 
 # height of tower and a counter for each floor
 num_floors = 63
+color = "gray"
 
 # iterate
 for floor in range(num_floors):
+    if floor % 21==0:
+        x = x + 75
+        y = y - 105
     # set placement and color of turtle
-    # changes the color every other floor
-
     painter.penup()
     painter.goto(x, y)
     if floor % 3 == 0:
-        painter.color("gray")
-    else:
-        painter.color("blue")
+        if color == "gray":
+            color = "blue"
+        else:
+            color = "gray"
+
+    painter.color(color)
     y = y + 5  # location of next floor
+
     # draw the floor
     painter.pendown()
     painter.forward(50)
