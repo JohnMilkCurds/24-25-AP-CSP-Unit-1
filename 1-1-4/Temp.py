0e8a095ccbfba17e31f84002e8a47e1cb990c974
15273 <ryan.mauro@vtsd.com>
2024-09-19 11:46:10 -0400

    Added alternating floor colors made 3 smaller towers.

---
+++ 1-1-4/Temp.py
@@ -0,0 +1,12 @@
+grade= input("What is the grade you got on the last test?")
+
+if grade > 90:
+    print("You got an A!")
+if grade > 80 and grade < 89:
+    print("You got an B!")
+if grade > 70 and grade <79:
+    print("You got an C!")
+if grade > 65 and grade < 69:
+    print("You got an D!")
+if grade < 64:
+    print("You are a disgrace to this family!")
