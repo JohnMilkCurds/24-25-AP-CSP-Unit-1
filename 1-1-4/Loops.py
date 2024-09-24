3e4bed234bb8f6c49ae4049f127e469474253f94
15273 <ryan.mauro@vtsd.com>
2024-09-24 12:29:10 -0400

    Added alternating floor colors made 3 smaller towers.

---
+++ 1-1-4/Loops.py
@@ -0,0 +1,8 @@
+num1=int(input("Enter a number: "))
+num2=int(input("Enter another number (All the math!!!)"))
+while num1!=num2:
+    print("You did not enter the same number")
+    print("Please enter a valid response")
+    num1 = int(input("Enter a number: "))
+    num2 = int(input("Enter another number (All the math!!!)"))
+print("You got it!")
---
+++ 1-1-4/count up.py
@@ -0,0 +1,12 @@
+
+num1=11
+num2=852
+print(num1)
+while (num1<num2):
+    num1=num1+11
+    while (num2<num1):
+        final= num1-11
+    print(num1)
+print(final)
+
+#tell the user the final number
