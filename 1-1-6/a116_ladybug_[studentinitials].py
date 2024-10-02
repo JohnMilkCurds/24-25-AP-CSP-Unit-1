04edbac319b82ce60985d4d1e611a54f266d5c72
15273 <ryan.mauro@vtsd.com>
2024-10-02 13:54:48 -0400

    Added alternating floor colors made 3 smaller towers. added legs

---
+++ 1-1-6/a116_ladybug_[studentinitials].py
@@ -0,0 +1,58 @@
+#   a116_ladybug.py
+import turtle as trtl
+
+# create ladybug head
+ladybug = trtl.Turtle()
+ladybug.pensize(40)
+ladybug.circle(5)
+
+# and body
+ladybug.penup()
+ladybug.goto(0, -55)
+ladybug.color("red")
+ladybug.pendown()
+ladybug.pensize(40)
+ladybug.circle(20)
+ladybug.setheading(270)
+ladybug.color("black")
+ladybug.penup()
+ladybug.goto(0, 5)
+ladybug.pensize(2)
+ladybug.pendown()
+ladybug.forward(75)
+
+# config dots
+num_dots = 1
+xpos = -20
+ypos = -55
+ladybug.pensize(10)
+
+# draw two sets of dots
+for line in range(2):
+  ladybug.penup()
+  ladybug.goto(xpos, ypos)
+  ladybug.pendown()
+  ladybug.circle(3)
+  ladybug.penup()
+  ladybug.goto(xpos + 30, ypos + 20)
+  ladybug.pendown()
+  ladybug.circle(2)
+
+  # position next dots
+  ypos = ypos + 25
+  xpos = xpos + 0
+  num_dot = num_dots + 1
+leg_num = 8
+leg_length = 150
+spacing = 260 / leg_num
+painter.pensize(5)
+n = 0
+while n < leg_num:
+  painter.goto(0,0)
+  painter.setheading(spacing*n)
+  painter.forward(leg_length)
+  n = n + 1
+ladybug.hideturtle()
+
+wn = trtl.Screen()
+wn.mainloop()
